a21d666555eeda44b5b47e78b4f3202c752f3110
farhan ibne saif <farhanibne860@gmail.com>
2023-03-12 13:38:36 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const iframe = document.querySelector('iframe');
+iframe.width=innerWidth;
+iframe.height=innerHeight;
+iframe.setAttribute('src', "https://ipfs.io/ipfs/QmTWsXeaoZ13WJY8psA2on9YQRhgPbNjWbi6VPYeu2xRFU/index.html")
